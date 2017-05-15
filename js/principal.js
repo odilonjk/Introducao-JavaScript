91b5a12d56a345f4a0082802c7ccbbc0fea78e3c
O.J. Kröger <krg@debian>
2017-05-15 20:44:22 -0300

    Melhorias no estilo da validação.

--- js/principal.js
+++ js/principal.js
@@ -2,20 +2,26 @@ var pacientes = document.querySelectorAll(".paciente");
 
 for(var i=0; i<pacientes.length;i++) {
 	var paciente = pacientes[i];
-
+	
 	var peso = paciente.querySelector(".info-peso").textContent;
 	var altura = paciente.querySelector(".info-altura").textContent;
 	tdIMC = paciente.querySelector(".info-imc");
+	
 	if(peso <= 0 || peso >= 1000) {
-		paciente.style.color = 'red';
-		paciente.style.fontWeight = 'bold';
+	
+		paciente.classList.add('paciente-invalido');
 		tdIMC.textContent = 'Peso inválido.';
+	
 	} else if(altura <= 0 || altura >= 3.00) {
-		paciente.style.color = 'red';
-		paciente.style.fontWeight = 'bold';
+	
+		paciente.classList.add('paciente-invalido');
 		tdIMC.textContent = 'Altura inválida.';
+	
 	} else {
+	
 		var imc = peso / (altura * altura);
 		tdIMC.textContent = imc.toFixed(2);
+	
 	}
+
 }
