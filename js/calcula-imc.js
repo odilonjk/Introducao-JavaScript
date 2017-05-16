b81ee68c1f992df79764c364bb191598ad08412a
O.J. Kröger <kroger.odilonj@gmail.com>
2017-05-16 20:35:24 -0300

    Iniciando refatoração do código.

---
+++ js/calcula-imc.js
@@ -0,0 +1,32 @@
+var titulo = document.querySelector("#titulo");
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i=0; i<pacientes.length;i++) {
+	var paciente = pacientes[i];
+
+	var peso = paciente.querySelector(".info-peso").textContent;
+	var altura = paciente.querySelector(".info-altura").textContent;
+	tdIMC = paciente.querySelector(".info-imc");
+
+	if(peso <= 0 || peso >= 1000) {
+
+		paciente.classList.add('paciente-invalido');
+		tdIMC.textContent = 'Peso inválido.';
+
+	} else if(altura <= 0 || altura >= 3.00) {
+
+		paciente.classList.add('paciente-invalido');
+		tdIMC.textContent = 'Altura inválida.';
+
+	} else {
+
+		var imc = peso / (altura * altura);
+		tdIMC.textContent = imc.toFixed(2);
+
+	}
+
+};
+
+titulo.addEventListener('click', function() {
+	window.location.reload(true);
+});
--- js/form.js
+++ js/form.js
@@ -1,38 +1,3 @@
-var titulo = document.querySelector("#titulo");
-var pacientes = document.querySelectorAll(".paciente");
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-
-
-for(var i=0; i<pacientes.length;i++) {
-	var paciente = pacientes[i];
-	
-	var peso = paciente.querySelector(".info-peso").textContent;
-	var altura = paciente.querySelector(".info-altura").textContent;
-	tdIMC = paciente.querySelector(".info-imc");
-	
-	if(peso <= 0 || peso >= 1000) {
-	
-		paciente.classList.add('paciente-invalido');
-		tdIMC.textContent = 'Peso inválido.';
-	
-	} else if(altura <= 0 || altura >= 3.00) {
-	
-		paciente.classList.add('paciente-invalido');
-		tdIMC.textContent = 'Altura inválida.';
-	
-	} else {
-	
-		var imc = peso / (altura * altura);
-		tdIMC.textContent = imc.toFixed(2);
-	
-	}
-
-};
-
-titulo.addEventListener('click', function() {
-	window.location.reload(true);
-});
-
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
