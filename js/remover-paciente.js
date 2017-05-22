aa3017be36289c5345742698d0a8f8e00a0321e0
odilonjk <kroger.odilonj@gmail.com>
2017-05-22 19:50:48 -0300

    Adicionado fade-out ao remover

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -5,9 +5,19 @@ var tabela = document.querySelector("table");
 //  O escutador do evento é a tabela
 tabela.addEventListener("dblclick", function(event) {
 
-  //  O alvo é o filho, mas então é chamado o pai
-  //  para realizar o remove() corretamente.
-  event.target.parentNode.remove();
+  event.target.parentNode.classList.add("fade-out");
+
+  //  Para o fade-out funcionar, é necessario usar
+  //  Timeout com espera de 500ms.
+  setTimeout(function() {
+
+    //  O target é o filho, então chamamos o pai
+    //  atravez de parentNode() para
+    //  realizar o remove() corretamente.
+    event.target.parentNode.remove();
+
+  }, 500);
+
 
   //  Disparando mensagem para o usuario
   enviaMensagem(["Paciente removido."]);
